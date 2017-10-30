63eacaf49e84d07dafce9b436125d8de5db6f867
Helcon Mabesa <helcon.mabesa@foxnews.com>
2017-10-30 17:21:42 -0400

    initial setup

--- src/page/index.js
+++ src/page/index.js
@@ -5,17 +5,20 @@ const config = require(process.env.CONFIG_PATH);
 const Helper = require(`${config.dir.src}/helper`);
 
 const Vue = require('vue');
-//const App = require('./App.vue');
 
-const app = new Vue({
-    template: `
-    <div id="app">
-        <h1>hello there, {{ foo }}</h1>
-    </div>
-    `,
-    data: {
-        foo: 'foobar'
-    }
-});
+const opts = {};
+
+opts.template = `
+<div id="app">
+    <h1>hello there, {{ foo }}</h1>
+</div>
+`;
+
+opts.data = {
+    foo: 'foobar'
+};
+
+
+const app = new Vue(opts);
 
 module.exports = app;
